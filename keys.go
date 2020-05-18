e890642e55a75e6276db8a99ed98653a5df98ef3
s3rj1k <evasive.gyron@gmail.com>
2020-05-18 21:15:14 +0300

    wip
    Signed-off-by: s3rj1k <evasive.gyron@gmail.com>

---
+++ keys.go
@@ -0,0 +1,114 @@
+package main
+
+import (
+	"bytes"
+	"context"
+	"fmt"
+	"log"
+	"os/exec"
+	"strconv"
+	"time"
+)
+
+const timeout = 5
+
+var (
+	sshKeyScanBinPath string
+	sshKeyGenBinPath  string
+)
+
+func init() {
+	var err error
+
+	if sshKeyScanBinPath, err = exec.LookPath("ssh-keyscan"); err != nil {
+		log.Fatal(err)
+	}
+
+	if sshKeyGenBinPath, err = exec.LookPath("ssh-keygen"); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func sshKeyFind(host string, port int) ([]byte, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), timeout*time.Second)
+	defer cancel()
+
+	var search string
+
+	if port > 0 {
+		search = fmt.Sprintf("[%s]:%d", host, port)
+	} else {
+		search = host
+	}
+
+	out, err := exec.CommandContext(
+		ctx,
+		sshKeyGenBinPath,
+		"-F", search,
+	).CombinedOutput()
+	if err != nil {
+		return nil, err
+	}
+
+	lines := bytes.Split(out, []byte("\n"))
+
+	var i int
+
+	for _, b := range lines {
+		if !bytes.HasPrefix(b, []byte("# ")) {
+			lines[i] = b
+			i++
+		}
+	}
+
+	lines = lines[:i]
+
+	return bytes.Join(lines, []byte("\n")), nil
+}
+
+func sshKeyScan(host string, port int) ([]byte, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), timeout*time.Second)
+	defer cancel()
+
+	args := make([]string, 0)
+	args = append(
+		args,
+		"-t", "rsa,dsa,ecdsa,ed25519",
+	)
+
+	if port > 0 {
+		args = append(
+			args,
+			"-p", strconv.Itoa(port),
+		)
+	}
+
+	args = append(
+		args,
+		host,
+	)
+
+	out, err := exec.CommandContext(
+		ctx,
+		sshKeyScanBinPath,
+		args...,
+	).CombinedOutput()
+	if err != nil {
+		return nil, err
+	}
+
+	lines := bytes.Split(out, []byte("\n"))
+
+	var i int
+
+	for _, b := range lines {
+		if !bytes.HasPrefix(b, []byte("# ")) {
+			lines[i] = b
+			i++
+		}
+	}
+
+	lines = lines[:i]
+
+	return bytes.Join(lines, []byte("\n")), nil
+}
---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"github.com/davecgh/go-spew/spew"
+)
+
+// ssh-keyscan -t rsa,dsa,ecdsa,ed25519 -p 2211 -H noc.mirohost.net
+
+/*
+root@salt:/srv/pillar/users# ssh-keygen -F [titan1.mirohost.net]:2211
+root@salt:/srv/pillar/users# ssh-keygen -F [titan1.mirohost.net]:22
+root@salt:/srv/pillar/users# ssh-keygen -F titan1.mirohost.net
+# Host titan1.mirohost.net found: line 14536
+|1|k0sOQHpR3q28P/opaWUUL1Dbxos=|88qaLm2ytZmTCDgvUDe5g4HuPo0= ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDQRCWwLzT5HTbbsPkHu9lY2Wm9nx3R5GmbZi6CKQy7SPBFlxrZ7dGOeXgWQyetGg8ukXKXDPygZhtjkRGEEEwOkVMUSI0wQfN6q+nkn9m5+q4n3dOKW0D49uvoYX8WxIOZ9Gq3I2us3LZlcJawWIeQRLbcjVYRf2EBTrcZPUV7xQAIvZs8dq48r3oXnN71XCE55G8jVWm5RS3l5ljqHE7UmqBMvZVy4UPlBvObS91HeERB6jI0HGQ23ScDro2x1AYAN7x7Baqx2Y7u3iXNX8RXsjrD4pploO37RN2MimCa0hAToOUGTL9QdLqhjymMAQD0YpMO0qFyK7vXn2fWKxzN
+*/
+
+func main() {
+	spew.Dump(sshKeyScan("noc.mirohost.net", 2211))
+	spew.Dump(sshKeyFind("noc.mirohost.net", 2211))
+}
+
+// type KnownHost struct {
+// 	Host string
+// 	Type string
+// 	Key  string
+// }
+
+// func toKnownHosts(b []byte) []KnownHost {
+
+// }
