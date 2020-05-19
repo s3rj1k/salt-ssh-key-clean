0a1116290d70435c148219f97d2cc45a26fccf0e
s3rj1k <evasive.gyron@gmail.com>
2020-05-19 15:59:30 +0300

    WiP
    Signed-off-by: s3rj1k <evasive.gyron@gmail.com>

--- keys.go
+++ keys.go
@@ -1,15 +1,25 @@
 package main
 
 import (
+	"bufio"
 	"context"
 	"fmt"
+	"io"
 	"log"
 	"os/exec"
+	"sort"
 	"strconv"
+	"strings"
 	"time"
 )
 
 const timeout = 5
+
+type knownHost struct {
+	Host string
+	Type string
+	Key  string
+}
 
 var (
 	sshKeyScanBinPath string
@@ -28,7 +38,7 @@ func init() {
 	}
 }
 
-func knownHostExecOutputWrapper(name string, args ...string) []KnownHost {
+func knownHostExecOutputWrapper(name string, args ...string) []knownHost {
 	ctx, cancel := context.WithTimeout(context.Background(), timeout*time.Second)
 	defer cancel()
 
@@ -50,7 +60,7 @@ func knownHostExecOutputWrapper(name string, args ...string) []KnownHost {
 
 	cmd.Start()
 
-	out := make([]KnownHost, 0)
+	out := make([]knownHost, 0)
 
 	for el := range toKnownHosts(stdoutPipe, stderrPipe) {
 		out = append(out, el)
@@ -61,7 +71,7 @@ func knownHostExecOutputWrapper(name string, args ...string) []KnownHost {
 	return out
 }
 
-func sshKeyFind(host string, port int) []KnownHost {
+func sshKeyFind(host string, port int) []knownHost {
 	var search string
 
 	if port > 0 {
@@ -73,7 +83,7 @@ func sshKeyFind(host string, port int) []KnownHost {
 	return knownHostExecOutputWrapper(sshKeyGenBinPath, "-F", search)
 }
 
-func sshKeyScan(host string, port int) []KnownHost {
+func sshKeyScan(host string, port int) []knownHost {
 	args := make([]string, 0)
 	args = append(
 		args,
@@ -93,4 +103,63 @@ func sshKeyScan(host string, port int) []KnownHost {
 	)
 
 	return knownHostExecOutputWrapper(sshKeyScanBinPath, args...)
+}
+
+func toKnownHosts(readers ...io.Reader) chan knownHost {
+	out := make(chan knownHost)
+	r := io.MultiReader(readers...)
+
+	go func(r io.Reader, out chan knownHost) {
+		scanner := bufio.NewScanner(r)
+
+		for scanner.Scan() {
+			line := scanner.Text()
+
+			if strings.HasPrefix(line, "# ") {
+				continue
+			}
+
+			fields := strings.Fields(line)
+			if len(fields) != 3 {
+				continue
+			}
+
+			out <- knownHost{
+				Host: fields[0],
+				Type: fields[1],
+				Key:  fields[2],
+			}
+		}
+
+		close(out)
+
+		if err := scanner.Err(); err != nil {
+			return
+		}
+	}(r, out)
+
+	return out
+}
+
+// https://github.com/golang/go/wiki/SliceTricks#in-place-deduplicate-comparable
+func deDuplicateKnownHosts(s []knownHost) []knownHost {
+	sort.Slice(
+		s, func(i, j int) bool {
+			return s[i].Type+s[i].Key < s[j].Type+s[j].Key
+		},
+	)
+
+	j := 0
+
+	for i := 1; i < len(s); i++ {
+		if s[i].Type+s[i].Key == s[j].Type+s[j].Key {
+			continue
+		}
+
+		j++
+
+		s[j] = s[i]
+	}
+
+	return s[:j+1]
 }
--- main.go
+++ main.go
@@ -1,24 +1,8 @@
 package main
 
 import (
-	"bufio"
-	"io"
-	"sort"
-
-	"strings"
-
 	"github.com/davecgh/go-spew/spew"
 )
-
-// ssh-keyscan -t rsa,dsa,ecdsa,ed25519 -p 2211 -H noc.mirohost.net
-
-/*
-root@salt:/srv/pillar/users# ssh-keygen -F [titan1.mirohost.net]:2211
-root@salt:/srv/pillar/users# ssh-keygen -F [titan1.mirohost.net]:22
-root@salt:/srv/pillar/users# ssh-keygen -F titan1.mirohost.net
-# Host titan1.mirohost.net found: line 14536
-|1|k0sOQHpR3q28P/opaWUUL1Dbxos=|88qaLm2ytZmTCDgvUDe5g4HuPo0= ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDQRCWwLzT5HTbbsPkHu9lY2Wm9nx3R5GmbZi6CKQy7SPBFlxrZ7dGOeXgWQyetGg8ukXKXDPygZhtjkRGEEEwOkVMUSI0wQfN6q+nkn9m5+q4n3dOKW0D49uvoYX8WxIOZ9Gq3I2us3LZlcJawWIeQRLbcjVYRf2EBTrcZPUV7xQAIvZs8dq48r3oXnN71XCE55G8jVWm5RS3l5ljqHE7UmqBMvZVy4UPlBvObS91HeERB6jI0HGQ23ScDro2x1AYAN7x7Baqx2Y7u3iXNX8RXsjrD4pploO37RN2MimCa0hAToOUGTL9QdLqhjymMAQD0YpMO0qFyK7vXn2fWKxzN
-*/
 
 func main() {
 	s := sshKeyScan("noc.mirohost.net", 2211)
@@ -30,69 +14,4 @@ func main() {
 	s = deDuplicateKnownHosts(s)
 
 	spew.Dump(s)
-}
-
-type KnownHost struct {
-	Host string
-	Type string
-	Key  string
-}
-
-func toKnownHosts(readers ...io.Reader) chan KnownHost {
-	out := make(chan KnownHost)
-	r := io.MultiReader(readers...)
-
-	go func(r io.Reader, out chan KnownHost) {
-		scanner := bufio.NewScanner(r)
-
-		for scanner.Scan() {
-			line := scanner.Text()
-
-			if strings.HasPrefix(line, "# ") {
-				continue
-			}
-
-			fields := strings.Fields(line)
-			if len(fields) != 3 {
-				continue
-			}
-
-			out <- KnownHost{
-				Host: fields[0],
-				Type: fields[1],
-				Key:  fields[2],
-			}
-		}
-
-		close(out)
-
-		if err := scanner.Err(); err != nil {
-			return
-		}
-	}(r, out)
-
-	return out
-}
-
-// https://github.com/golang/go/wiki/SliceTricks#in-place-deduplicate-comparable
-func deDuplicateKnownHosts(s []KnownHost) []KnownHost {
-	sort.Slice(
-		s, func(i, j int) bool {
-			return s[i].Type+s[i].Key < s[j].Type+s[j].Key
-		},
-	)
-
-	j := 0
-
-	for i := 1; i < len(s); i++ {
-		if s[i].Type+s[i].Key == s[j].Type+s[j].Key {
-			continue
-		}
-
-		j++
-
-		s[j] = s[i]
-	}
-
-	return s[:j+1]
 }
